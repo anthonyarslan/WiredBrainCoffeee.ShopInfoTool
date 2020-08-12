520584ba2306ef853964f773259af0e76a197731
Tony Arslan <tony.arslan@educationadvanced.com>
2020-08-12 16:20:48 -0500

    Add project files.

---
+++ WiredBrainCoffeee.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffeee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffeee - Shop Info Tool");
+        }
+    }
+}
