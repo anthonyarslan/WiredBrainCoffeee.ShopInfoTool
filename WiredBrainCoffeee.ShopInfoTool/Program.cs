f791d1ea81316a766d8ffbc375fef91831b732f8
Tony Arslan <tony.arslan@educationadvanced.com>
2020-08-13 12:22:11 -0500

    Introduce Command Handlers

--- WiredBrainCoffeee.ShopInfoTool/Program.cs
+++ WiredBrainCoffeee.ShopInfoTool/Program.cs
@@ -7,35 +7,32 @@ namespace WiredBrainCoffeee.ShopInfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
-
-            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
-              "write 'quit' to exit application");
-
-            var coffeeShopDataProvider = new CoffeeShopDataProvider();
-
-            while (true)
+            static void Main(string[] args)
             {
-                var line = Console.ReadLine();
+                Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+                  "write 'quit' to exit application");
+
+                var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+                while (true)
                 {
-                    break;
-                }
+                    var line = Console.ReadLine();
 
-                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
-
-                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("> Available coffee shop commands:");
-                    foreach (var coffeeShop in coffeeShops)
+                    if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
                     {
-                        Console.WriteLine($"> " + coffeeShop.Location);
+                        break;
                     }
-                }
-                else
-                {
-                    Console.WriteLine($"> Tony, You are the greatest!" );
+
+                    var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                    var commandHandler =
+                      string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                      ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                      : new CoffeeShopCommandHandler(coffeeShops, line);
+
+                    commandHandler.HandleCommand();
                 }
             }
         }
